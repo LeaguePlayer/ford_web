a253fa6bab9a5af6cd17c542c4136592819c7261
leagueplayer <four.ibm-knight@hotmail.com>
2013-08-29 15:59:53 +0600

    new3

--- themes/default/views/staticpage/view.php
+++ themes/default/views/staticpage/view.php
@@ -8,28 +8,28 @@
         </section>
     <? }?>
 
-<section class="discount fix_width">
-	<h3><?=$model->title?></h3>
-	<div class="ford">
-		
-		<?=$model->html_content?>
-        
-        <? if( $show_interest ) { ?>
-        	<? if( $model->id_car ) { ?>
-                <div class="intrest">
-                    <h2>Заинтересовало предложение?</h2><a href="/cars/<?=$model->id_car?>" class="green_button">Перейти к предложению</a>
-                </div>
+    <section class="discount fix_width">
+        <h3><?=$model->title?></h3>
+        <div class="ford">
+            
+            <?=$model->html_content?>
+            
+            <? if( $show_interest ) { ?>
+                <? if( $model->id_car ) { ?>
+                    <div class="intrest">
+                        <h2>Заинтересовало предложение?</h2><a href="/cars/<?=$model->id_car?>" class="green_button">Перейти к предложению</a>
+                    </div>
+                <? } ?>
             <? } ?>
-        <? } ?>
+            
+        </div>
         
-	</div>
-	
-</section>
+    </section>
 
-<!--END-content-->
-<? if(!Yii::app()->request->isAjaxRequest) {?>
-	<section class="site_map four">
-			<?=$this->renderPartial('/menu/list_menu',array('menu'=>$data['menu']))?>
-	</section>
-<? } ?>
+    <!--END-content-->
+    <? if(!Yii::app()->request->isAjaxRequest) {?>
+        <section class="site_map four">
+                <?=$this->renderPartial('/menu/list_menu',array('menu'=>$data['menu']))?>
+        </section>
+    <? } ?>
 </div>
